e93cc505eabd38e8cf31659dd7f3d9152b092c02
fly <rawoot.p@gmail.com>
2017-05-22 07:47:43 +0000

    Initial commit.

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("com.cds.demo01cdsdemo01.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("com.cds.demo01cdsdemo01.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
